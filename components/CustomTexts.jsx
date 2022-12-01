b7806d863e3ce55abafd860cb1c139b71d515b78
AakashJyoti <akjyoti572@gmail.com>
2022-12-01 23:28:06 +0530

    quotes issue resolved

--- components/CustomTexts.jsx
+++ components/CustomTexts.jsx
@@ -1,13 +1,10 @@
-"use client";
+'use client';
 
-import { motion } from "framer-motion";
-import styles from "../styles";
+import { motion } from 'framer-motion';
 import {
-  fadeIn,
   textContainer,
-  staggerContainer,
   textVariant2,
-} from "../utils/motion";
+} from '../utils/motion';
 
 export const TypingText = ({ title, textStyles }) => (
   <motion.p
@@ -16,10 +13,10 @@ export const TypingText = ({ title, textStyles }) => (
   >
     {Array.from(title).map((letter, index) => (
       <motion.span variants={textVariant2} key={index}>
-        {letter === " " ? "\u00A0" : letter}
+        {letter === ' ' ? '\u00A0' : letter}
       </motion.span>
     ))}
   </motion.p>
 );
 
-export const TitleText = ({ title, textStyles }) => <h2>Title Text</h2>;
+// export const TitleText = ({ title, textStyles }) => <h2>Title Text</h2>;
