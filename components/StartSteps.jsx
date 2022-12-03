3913b2eef1756bbc97d20b174eeefa92c128e6d9
AakashJyoti <akjyoti572@gmail.com>
2022-12-03 23:20:28 +0530

    font issue solved

--- components/StartSteps.jsx
+++ components/StartSteps.jsx
@@ -6,10 +6,10 @@ const StartSteps = ({ number, text }) => (
       className={`${styles.flexCenter} w-[70px] h-[70px] rounded-[24px] bg-[#323f5d]`}
     >
       <p className="font-bold text-[20px]">0{number}</p>
-      <p className="flex-1 ml-[30px] font-normal text-[18px] text-[#B0B0B0] leading-[32px]">
-        {text}
-      </p>
     </div>
+    <p className="flex-1 ml-[30px] font-normal text-[18px] text-[#B0B0B0] leading-[32px]">
+      {text}
+    </p>
   </div>
 );
 
